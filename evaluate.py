1b919bb1582a48419bb414f6cdaae75e935e3a56
ltduy6 <lamthanhduy2k4@gmail.com>
2025-12-02 08:00:12 +0700

    update

--- evaluate.py
+++ evaluate.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     model = LanGuideMedSegWrapper(args)
 
     torch.serialization.add_safe_globals([config.CfgNode])
-    checkpoint = torch.load('./save_model/medseg.ckpt',map_location='cpu')["state_dict"]
+    checkpoint = torch.load('./save_model/medseg.ckpt',map_location='cpu',weight_only=False)["state_dict"]
     model.load_state_dict(checkpoint,strict=True)
 
     # dataloader
