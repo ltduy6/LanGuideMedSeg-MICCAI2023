a193f2741c7e2a628973a2a2ff7208dc96a111bb
ltduy6 <lamthanhduy2k4@gmail.com>
2025-12-30 09:25:10 +0700

    test nvida/mit-b2 as vision encoder

--- utils/model.py
+++ utils/model.py
@@ -4,7 +4,7 @@ from einops import rearrange, repeat
 from .layers import GuideDecoder
 from monai.networks.blocks.dynunet_block import UnetOutBlock
 from monai.networks.blocks.upsample import SubpixelUpsample
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, SegFormerModel
 
 
 
@@ -40,17 +40,15 @@ class VisionModel(nn.Module):
     def __init__(self, vision_type, project_dim):
         super(VisionModel, self).__init__()
 
-        self.model = AutoModel.from_pretrained(vision_type,output_hidden_states=True)   
-        self.project_head = nn.Linear(768, project_dim)
-        self.spatial_dim = 768
+        self.model = SegFormerModel.from_pretrained(vision_type,output_hidden_states=True)   
+        self.project_head = nn.Linear(512, project_dim)
 
     def forward(self, x):
 
         output = self.model(x, output_hidden_states=True)
-        embeds = output['pooler_output'].squeeze()
-        project = self.project_head(embeds)
+        hidden_states = output.hidden_states[1:]
 
-        return {"feature":output['hidden_states'], "project":project}
+        return {"feature":hidden_states}
 
 
 class LanGuideMedSeg(nn.Module):
@@ -62,13 +60,13 @@ class LanGuideMedSeg(nn.Module):
         self.encoder = VisionModel(vision_type, project_dim)
         self.text_encoder = BERTModel(bert_type, project_dim)
 
-        self.spatial_dim = [7,14,28,56]    # 224*224
-        feature_dim = [768,384,192,96]
+        self.spatial_dim = [56, 28, 14, 7]    # 224*224
+        feature_dim = [64, 128, 320, 512]
 
-        self.decoder16 = GuideDecoder(feature_dim[0],feature_dim[1],self.spatial_dim[0],24)
-        self.decoder8 = GuideDecoder(feature_dim[1],feature_dim[2],self.spatial_dim[1],12)
-        self.decoder4 = GuideDecoder(feature_dim[2],feature_dim[3],self.spatial_dim[2],9)
-        self.decoder1 = SubpixelUpsample(2,feature_dim[3],24,4)
+        self.decoder16 = GuideDecoder(feature_dim[3],feature_dim[2],self.spatial_dim[3],24)
+        self.decoder8 = GuideDecoder(feature_dim[2],feature_dim[1],self.spatial_dim[2],12)
+        self.decoder4 = GuideDecoder(feature_dim[1],feature_dim[0],self.spatial_dim[1],9)
+        self.decoder1 = SubpixelUpsample(2,feature_dim[0],24,4)
         self.out = UnetOutBlock(2, in_channels=24, out_channels=1)
 
     def forward(self, data):
@@ -78,19 +76,15 @@ class LanGuideMedSeg(nn.Module):
             image = repeat(image,'b 1 h w -> b c h w',c=3)
 
         image_output = self.encoder(image)
-        image_features, image_project = image_output['feature'], image_output['project']
+        image_features = image_output['feature'], image_output['project']
         text_output = self.text_encoder(text['input_ids'],text['attention_mask'])
         text_embeds, text_project = text_output['feature'],text_output['project']
-
-        if len(image_features[0].shape) == 4: 
-            image_features = image_features[1:]  # 4 8 16 32   convnext: Embedding + 4 layers feature map
-            image_features = [rearrange(item,'b c h w -> b (h w) c') for item in image_features] 
 
         os32 = image_features[3]
         os16 = self.decoder16(os32,image_features[2], text_embeds[-1])
         os8 = self.decoder8(os16,image_features[1], text_embeds[-1])
         os4 = self.decoder4(os8,image_features[0], text_embeds[-1])
-        os4 = rearrange(os4, 'B (H W) C -> B C H W',H=self.spatial_dim[-1],W=self.spatial_dim[-1])
+        os4 = rearrange(os4, 'B (H W) C -> B C H W',H=self.spatial_dim[0],W=self.spatial_dim[0])
         os1 = self.decoder1(os4)
 
         out = self.out(os1).sigmoid()
